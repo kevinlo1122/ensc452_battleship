7049ae00eeaa4a254ca87645c7021a27a55bc74a
kevinlo1122 <45241916+kevinlo1122@users.noreply.github.com>
2023-04-02 15:49:32 -0700

    Update axi.vhd

--- axi.vhd
+++ axi.vhd
@@ -5,3 +5,12 @@
       b: in std_logic_VECTOR(15 downto 0);
       p: out std_logic_VECTOR(31 downto 0));
     end component;
+
+        
+        
+    RNG_0 : RNG
+    port map (
+      clk => S_AXI_ACLK,
+      a => slv_reg0(31 downto 16),
+      b => slv_reg0(15 downto 0),
+      p => RNG_out);
